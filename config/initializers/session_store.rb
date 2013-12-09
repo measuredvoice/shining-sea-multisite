3382902f5a8c4051111263af6537cd9c1994a78a
Chris Radcliff <chris.radcliff@gmail.com>
2013-12-09 12:51:20 -0800

    This is a Rails 3.2.x application.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShiningSeaMultisite::Application.config.session_store :cookie_store, key: '_shining-sea-multisite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShiningSeaMultisite::Application.config.session_store :active_record_store
