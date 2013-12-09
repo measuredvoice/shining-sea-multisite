00d1ad0188ee31037e4546dcf9fe15fcf5304ec6
Chris Radcliff <chris.radcliff@gmail.com>
2013-12-09 15:34:24 -0800

    Added basic rails_admin support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ShiningSeaMultisite::Application.routes.draw do
   devise_for :users
+  
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   # Default to the documentation and examples
   root :to => 'documentation#index'
